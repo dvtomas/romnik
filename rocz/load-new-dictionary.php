ea6a081cb432e064b1c354b6efdbb9bedf17ca5c
Tomas Dvorak <dvtomas@gmail.com>
2012-04-16 08:06:23 +0200

    Added footer.php

--- rocz/load-new-dictionary.php
+++ rocz/load-new-dictionary.php
@@ -28,7 +28,8 @@
     <link rel="stylesheet" href="/css/blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
 
     <!-- Import fancy-type plugin for the sample page. -->
-    <link rel="stylesheet" href="/css/blueprint/plugins/fancy-type/screen.css" type="text/css" media="screen, projection">
+    <link rel="stylesheet" href="/css/blueprint/plugins/fancy-type/screen.css" type="text/css"
+          media="screen, projection">
     <link rel="stylesheet" href="../css/style.css">
 
     <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
@@ -100,39 +101,6 @@ chromium.org/developers/how-tos/chrome-frame-getting-started -->
             dumpParseResult();
         }
     }
-    ?>
-    <div class="reklama_endora">
-        <hr/>
-        <endora/>
-    </div>
 
-</div>
-<footer>
-
-</footer>
-<!-- JavaScript at the bottom for fast page loading -->
-
-<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="../js/libs/jquery-1.7.1.min.js"><\/script>')</script>
-
-<!-- scripts concatenated and minified via build script -->
-<script src="../js/plugins.js"></script>
-<script src="../js/script.js"></script>
-<!-- end scripts -->
-
-<!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
-mathiasbynens.be/notes/async-analytics-snippet -->
-<script>
-    var _gaq = [
-        ['_setAccount', 'UA-XXXXX-X'],
-        ['_trackPageview']
-    ];
-    (function (d, t) {
-        var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
-        g.src = ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
-        s.parentNode.insertBefore(g, s)
-    }(document, 'script'));
-</script>
-</body>
-</html>
+    include "../lib/footer.php"
+?>
